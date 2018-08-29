321033e9e790d97ede720adc40fe936b5e196474
travisgdesir31@gmail.com <travisgdesir31@gmail.com>
2018-08-29 04:09:12 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,22 @@
-//define functions here
-
+function getIt(){
+   $('p').on("click",function(){
+    alert("Hey!")
+  })
+}
+function frameIt(){
+   $('img').on("load", function(){
+     $('img').append('.tasty')
+  })
+}
+function pressIt(){
+   $("form").on('keydown',function(e){
+    if (e.which === '71'){
+      alert(" you pressed g")
+    }
+  })
+}
 $(document).ready(function(){
-
+getit()
 // call functions here
 
 });
