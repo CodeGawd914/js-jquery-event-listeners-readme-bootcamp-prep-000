1fb26900f839943c3fdb98d11e3b97335f89f25e
travisgdesir31@gmail.com <travisgdesir31@gmail.com>
2018-08-29 04:16:24 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -18,7 +18,7 @@ function pressIt(){
 function submitIt(){
   $("form").on("submit",function (){
     if ($("input:first" ).val()==="correct"){
-      window.alert()
+      window.alert("Your form is going to be submitted now.")
     }
   })
 }
